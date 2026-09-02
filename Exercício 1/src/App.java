58d20acebbe24f814966096172cdface9a308b83
loriisduartte <lorena.duarte2404@gmail.com>
2026-09-02 20:44:34 +0000

    Exercício 1

---
+++ Exercício 1/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        String nome = "Lorena Duarte";
+        int idade = 29;
+        String curso = "Engenharia de Software";
+        int periodo = 1;
+        String matricula = "Ativa";
+        String Ficha = "======Ficha do Estudante=======";
+
+       
+       System.out.println (Ficha);
+        System.out.println ("Nome: " + nome);
+        System.out.println ("Idade: " + idade);
+        System.out.println ("Curso: " + curso);
+        System.out.println ("Periodo: " + periodo);
+        System.out.println ("Matricula: " + matricula);
+    }
+}
